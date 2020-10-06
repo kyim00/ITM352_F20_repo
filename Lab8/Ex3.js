cc48844e2a423ea258acd51f606f848527f20979
kyim00 <kcyim@hawaii.edu>
2020-10-06 08:29:21 -1000

    up

---
+++ Lab8/Ex3.js
@@ -0,0 +1,6 @@
+age_count = 1; // start the age count
+age = 20;
+for (var age_count = 50; age_count > age; age_count--) {
+    console.log(`age ${age_count}`);
+}
+console.log (`Kimberly is ${age_count} years old`)
